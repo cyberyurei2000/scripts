ba4d8e39a26f5eabd4da3240659c1a45aea81516
cyberyurei2000 <61709133+cyberyurei2000@users.noreply.github.com>
2024-06-14 05:14:25 -0300

    [WIP] Add a quick setup script for Windows

---
+++ install.ps1
@@ -0,0 +1,17 @@
+# Copyright (c) cyberyurei2000 2024
+# Released under the BSD 3-Clause License
+# https://opensource.org/license/bsd-3-clause
+
+$Dir = [Environment]::CurrentDirectory
+$IsScriptPath = Test-Path -Path "C:\bin\scripts"
+
+function Set-Scripts {
+    if($null -eq $IsScriptPath) {
+        New-Item -Path "C:\bin" -Name "scripts" -ItemType "Directory" -Force
+    }
+    $BinPath = "C:\bin\scripts"
+
+    Copy-Item -Path "$Dir\bin\windows\vcr.ps1" -Destination "$BinPath\vcr.ps1" -Force
+}
+
+#Set-Scripts
